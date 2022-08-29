f64032dfb8fb14d1a228af361a55fa1e4ac419ed
Robert Bittick <robertbittick@gmail.com>
2022-08-29 00:06:23 -0700

    commit tree 89a1f91caf989834dde0944389fc22f11c4a6c7f

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string) {
+    return string.toUpperCase("Hello");
+}
+
+function whisper(string) {
+    return string.toLowerCase("Hello");
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase("Hello"))
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase("Hello"))
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === "hello")
+     return "I can\'t hear you!"
+        else if (string === "HELLO")
+    return "YES INDEED!"
+        else (string === "Let's have dinner together!")
+        return "I would love to!"
+}
